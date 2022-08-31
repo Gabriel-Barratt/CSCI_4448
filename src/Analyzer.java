1faa8fbc4197fedc2a8d315c4cf0e696cea7b8ec
Gabriel-Barratt <77457314+Gabriel-Barratt@users.noreply.github.com>
2022-08-31 09:55:17 -0600

    Initial commit

--- src/Analyzer.java
+++ src/Analyzer.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class Analyzer
 {
-    static String read()
-    {
+    static String read()        //runs the other functions and prints it out
+    {                           //to look nice and neat
         double[] lst = new double[0];
         Scanner reader = new Scanner(System.in);
 
         System.out.println("Enter a number: ");
-        String input = reader.nextLine();
+        String input = reader.nextLine();       //storing user input
         lst = Arrays.copyOf(lst, lst.length +1);
         lst [lst.length - 1] = Double.parseDouble(input);
         System.out.println(Arrays.toString(lst));
 
-        while(!(input.equals("")))
+        while(!(input.equals("")))     //had to find a way to stop the program when nothing was entered
         {
             System.out.println("Enter a number: ");
             input = reader.nextLine();
@@ -38,14 +38,15 @@ public class Analyzer
         reader.close();
         return input;
     }
+
     //source: https://www.geeksforgeeks.org/median/
     static double median(double[] lst)
     {
-        Arrays.sort(lst);
+        Arrays.sort(lst);           //sort the array
         double median;
 
-        if(lst.length % 2 == 0)
-        {
+        if(lst.length % 2 == 0)     //checking if the array is even or odd
+        {                           //then computing the median
             median = (lst[((lst.length)/2)] + lst[((lst.length) / 2) - 1]) / 2;
             return median;
         }
@@ -55,41 +56,42 @@ public class Analyzer
             return median;
         }
     }
+
     static double mean(double[] lst)
     {
-        Arrays.sort(lst);
         double mean;
         double total = 0;
-        for(int i = 0; i < lst.length; i++)
+        for(int i = 0; i < lst.length; i++) //Go through the array add up all the values
         {
             total += lst[i];
         }
-        mean = total / lst.length;
+        mean = total / lst.length;      //divide the sum by the length to get the mean
         return mean;
     }
+
     // https://stackoverflow.com/questions/8858327/finding-multiple-modes-in-an-array
     static ArrayList<Double> mode(double[] temp)
     {
-        ArrayList<Double> mode = new ArrayList<Double>();
+        ArrayList<Double> mode = new ArrayList<Double>();   //creating an arraylist for the mode
         double counter = 0;
         double counter_2 = 0;
         for (int i = 0; i < temp.length; i++)
         {
             counter_2 = 0;
-            for (int j = 0; j < temp.length; j++)
-            {
+            for (int j = 0; j < temp.length; j++)   //nested loop that allows us to go through the
+            {                                       //array and check/count each value
                 if (temp[i] == temp[j])
                 {
                     counter_2++;
                 }
             }
-            if (counter < counter_2 && !mode.contains(temp[i]))
+            if (counter < counter_2 && !mode.contains(temp[i])) //adding the mode to the arraylist
             {
                 counter = counter_2;
                 mode.clear();
                 mode.add(temp[i]);
             }
-            else if(counter == counter_2 && !mode.contains(temp[i]))
+            else if(counter == counter_2 && !mode.contains(temp[i]))    //checking for multiple modes
             {
                 mode.add(temp[i]);
             }
@@ -104,13 +106,15 @@ public class Analyzer
         double total = 0;
         double mean = 0;
 
-        for (int i = 0; i < lst.length; i++)
+        for (int i = 0; i < lst.length; i++)        //getting the sum of all the values
         {
             total += lst[i];
         }
-        mean = total / lst.length;                      // Compute sum squared differences with the mean
+        mean = total / lst.length;                  //divide the sum by the length to get the mean
         for (int i = 0; i < lst.length; i++)
+        {
             variance += Math.pow((lst[i] - mean), 2);    //variance math
+        }
         return variance / lst.length;
     }
 
@@ -122,35 +126,35 @@ public class Analyzer
         double square_diff = 0;
         double var = 0;
 
-        for (int i = 0; i < lst.length; i++)
+        for (int i = 0; i < lst.length; i++)    //getting the sum of values
         {
             total += lst[i];
         }
-        mean = total / lst.length;
+        mean = total / lst.length;          //finding the mean
 
         for (int i = 0; i < lst.length; i++)
         {
-            square_diff += Math.pow((lst[i] - mean), 2);    //takes the power
+            square_diff += Math.pow((lst[i] - mean), 2);    //standard deviation math
         }
         var = square_diff / lst.length;
-        standard_dev = Math.sqrt(var);           //takes the square root of the std
+        standard_dev = Math.sqrt(var);           //takes the square root of the std and returns
         return standard_dev;
     }
 
-    static double min(double[] lst)
+    static double min(double[] lst)         //sorts the array and takes the first value in it
     {
         Arrays.sort(lst);
         return lst[0];
     }
 
-    static double max(double[] lst)
+    static double max(double[] lst)         //sorts the array and takes the last value in it
     {
         Arrays.sort(lst);
         return lst[lst.length -1];
     }
 
-    static double max_occurrences(double[] temp)
-    {
+    static double max_occurrences(double[] temp)        //same logic as the mode, but now we return
+    {                                                   //the second counter
         ArrayList<Double> mode = new ArrayList<Double>();
         double counter = 0;
         double counter_2 = 0;
--- src/Reader.java
+++ src/Reader.java
@@ -3,22 +3,22 @@ import java.util.Arrays;
 
 public class Reader
 {
-    static String read()
-    {
+    static String read()            //runs the other functions and prints it out
+    {                               //to look nice and neat
         Scanner read = new Scanner(System.in);
         print("Enter a string to read: ");
-        String user_input = read.nextLine();
+        String user_input = read.nextLine();    //storing user input
         read.close();
 
         print(user_input);
 
-        user_input = clean(user_input);
+        user_input = clean(user_input);         //clean function
         print(user_input);
 
-        user_input = sort(user_input);
+        user_input = sort(user_input);          //sort function
         print(user_input);
 
-        user_input = palindrome(user_input);
+        user_input = palindrome(user_input);    //palindrome function
         print(user_input);
 
         return user_input;
@@ -27,24 +27,24 @@ public class Reader
     //https://www.geeksforgeeks.org/how-to-remove-all-white-spaces-from-a-string-in-java/
     static String clean(String args)
     {
-        args = args.toUpperCase();
-        args = args.replaceAll("\\s", "");
+        args = args.toUpperCase();      //converts everything to uppercase
+        args = args.replaceAll("\\s", "");  //replaces all the spaces with nothing
         return args;
     }
 
     //https://www.geeksforgeeks.org/sort-a-string-in-java-2-different-ways/
     static String sort(String args)
     {
-        char tempArray[] = args.toCharArray();
-        Arrays.sort(tempArray);
+        char[] tempArray = args.toCharArray();      //creating a temp character array that uses the
+        Arrays.sort(tempArray);                     //sort function and returns it as a string
         return new String (tempArray);
     }
 
     //https://stackoverflow.com/questions/13827654/how-can-i-get-a-char-array-in-reverse-order
     //https://www.geeksforgeeks.org/java-util-arrays-copyofrange-java/
     static String palindrome(String args)
-    {
-        char[] breakup = args.toCharArray();
+    {                                           //creating a character array, breaking up the word,
+        char[] breakup = args.toCharArray();    //reversing half of it, and splicing it together
         char[] erase = Arrays.copyOfRange(breakup, 1, breakup.length);
         char[] flip = (new StringBuilder(new String(args)).reverse().toString()).toCharArray();
 
@@ -59,5 +59,5 @@ public class Reader
     static void print(String args)
     {
         System.out.println(args);
-    }
-}
+    }   //used so i don't have to keep
+}                                                                  //typing system.out.println
