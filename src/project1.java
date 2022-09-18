27c656d11c47a2337572f4fa6d5d49e1919baefa
Gabriel-Barratt <77457314+Gabriel-Barratt@users.noreply.github.com>
2022-09-18 15:33:58 -0600

    project2.2

--- src/project1.java
+++
@@ -1,9 +0,0 @@
-public class project1
-{
-    public static void main(String[] args)
-    {
-        Analyzer equation = new Analyzer();
-        equation.read();
-    }
-
-}
--- src/project1_strings.java
+++
@@ -1,9 +0,0 @@
-public class project1_strings
-{
-    public static void main(String[] args)
-    {
-        Reader text = new Reader();
-        text.read();
-    }
-
-}
